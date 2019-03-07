53e8a58e7c5f215d6fb0b225f608942f491c9ad3
118400186 <118400186@BISBIS-1LV0EGOM.bis.ad.ucc.ie>
2019-03-07 09:32:40 +0000

    Revert ""
    This reverts commit 1c6857d2ab76072efd8fb5289540321cccedad72.

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
- 
-
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -9,43 +7,11 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Menu</title>
-        <style>
-            .button {
-                background-color: #867979; /* Black */
-                border: none;
-                color: White;
-                padding: 15px 32px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                margin: 4px 2px;
-                cursor: pointer;
-                
-            }
-            
-            .button {font-size: 75px;}
-            body {
-                background-size: 100%;
-            }
-            
-            .button:hover {
-                background-color: #300
-            }
-            h1 {
-                font-size: 75px;
-            }
-        </style>
+        <title></title>
     </head>
-    <body background="img/watercolour.jpg">
-    <center>
-        <h1>Welcome</h1>
-      
-        <a href="cv.html"><button type="button" class="button">CV</button></a>
-        <br><br><br><br><br><br>
-        <a href="tiltedpage_scroll_demo.html"><button type="button" class="button">Interests</button></a>
-        <br><br><br><br><br><br>
-        <a href="ConsultingHomepage.html"><button type="button" class="button">Consulting Services</button></a>
-    </center>
+    <body>
+        <?php
+        echo 'HEllo World'; //How to print words in PHP
+        ?>
     </body>
 </html>
